d159049147cd2ccb19f5e90dbcaba848c8e1c182
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-29 18:23:17 +0530

    Capturing and Storing User's Details

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,24 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import Login from './login';
 import Loading from '../componnets/Loading';
+import { useEffect } from 'react';
+import firebase from 'firebase';
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set(
+        {
+          name: user.displayName,
+          email: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
   if (loading) return <Loading />;
   if (!user) return <Login />;
   return <Component {...pageProps} />;
