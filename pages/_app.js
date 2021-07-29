cfb6a8cae13fca1629aa26fa47da0d4be223e102
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-29 17:54:13 +0530

    Loading Component

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import '../styles/globals.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import Login from './login';
+import Loading from '../componnets/Loading';
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  if (loading) return <Loading />;
   if (!user) return <Login />;
   return <Component {...pageProps} />;
 }
