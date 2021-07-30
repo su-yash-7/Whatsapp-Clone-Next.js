32373ddbb153a1677a3aabb565821d3690668849
su-yash-7 <suyashpradhan7@gmail.com>
2021-07-30 15:27:43 +0530

    [id] Component

---
+++ componnets/ChatScreen.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+function ChatScreen() {
+  return <Container>hello ChatScreen</Container>;
+}
+
+export default ChatScreen;
+const Container = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,9 +1,30 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+import ChatScreen from '../../componnets/ChatScreen';
+import Sidebar from '../../componnets/Sidebar';
 function Chat() {
   return (
-    <div>
-      <h1>Chat Page</h1>
-    </div>
+    <Container>
+      <Head>
+        <title>Chat</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen />
+      </ChatContainer>
+    </Container>
   );
 }
 
 export default Chat;
+const Container = styled.div`
+  display: flex;
+`;
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
