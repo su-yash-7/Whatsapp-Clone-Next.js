1f341d2acdbed1d8e226dce06496a22fe2bf6d7b
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-01 10:26:18 +0530

    Chat Screen Header

--- componnets/ChatScreen.js
+++ componnets/ChatScreen.js
@@ -1,8 +1,59 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from 'styled-components';
+import { auth } from '../firebase';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-function ChatScreen() {
-  return <Container>hello ChatScreen</Container>;
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>recipientEmail</h3>
+          <p>Last Seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+    </Container>
+  );
 }
 
 export default ChatScreen;
 const Container = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+const HeaderIcons = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -44,7 +44,6 @@ export async function getServerSideProps(context) {
     id: chatRes.id,
     ...chatRes.data(),
   };
-  console.log(chat, messages);
   return {
     props: {
       messages: JSON.stringify(messages),
